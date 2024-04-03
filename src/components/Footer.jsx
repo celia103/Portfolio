68d7a932d5a50a99cf4e17738d37141a98efd804
celia103 <celiayych@gmail.com>
2024-04-03 19:43:22 +0100

    fix footer layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,23 @@
 //Footer.jsx
 import React from "react";
-import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-	faLinkedin,
 	faGithub,
 	faLinkedinIn,
 } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-library.add(faLinkedin);
 
 function Footer() {
 	return (
 		<div className="container">
 			<footer className="d-flex flex-wrap justify-content-between align-items-center px-4 py-3 mt-5 border-top">
-				<div className="col-md-4 d-flex align-items-center">
-					<span class="mb-3 mb-md-0 text-body-secondary">
+				<div className="col-md-5 py-auto">
+					<span className="mb-3 mb-md-0 text-body-secondary">
 						&copy; 2024 Celia&rsquo;s Portfolio
 					</span>
 				</div>
-				<ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-					<li class="ms-3">
+				<ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+					<li className="ms-3">
 						<a
 							href="https://www.linkedin.com/in/celiacyy/"
 							target="_blank"
@@ -30,7 +27,7 @@ function Footer() {
 							<FontAwesomeIcon icon={faLinkedinIn} />
 						</a>
 					</li>
-					<li class="ms-3">
+					<li className="ms-3">
 						<a
 							href="mailto:celiayych@gmail.com"
 							target="_blank"
@@ -40,7 +37,7 @@ function Footer() {
 							<FontAwesomeIcon icon={faEnvelope} />
 						</a>
 					</li>
-					<li class="ms-3">
+					<li className="ms-3">
 						<a
 							href="https://github.com/celia103"
 							target="_blank"
